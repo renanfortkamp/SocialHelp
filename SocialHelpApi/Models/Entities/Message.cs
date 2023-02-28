adf135aa45d47cefa17d3191b7af80768c7dcf71
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2023-02-28 00:34:37 -0300

    Models iniciais

---
+++ SocialHelpApi/Models/Entities/Message.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialHelpApi.Models.Entities
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Text { get; set; }
+        public DateTime DateMessage { get; set; } = DateTime.Now;
+        public int EnumStatus { get; set; } = 1;
+        public bool Edit { get; set; } = false;
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+    }
+}
---
+++ SocialHelpApi/Models/Entities/User.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialHelpApi.Models.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }      
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }        
+
+        [Required]
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+        
+    }
+}
