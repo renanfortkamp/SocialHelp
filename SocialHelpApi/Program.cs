46e8d63e704e367f8a85798acff41b8dedf58631
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2023-02-28 21:28:08 -0300

    injetando automapper e contexto na api

--- SocialHelpApi/Program.cs
+++ SocialHelpApi/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using SocialHelpApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ContextApi>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("ServerConnection"))
+);
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
