0cdd0b8fa440aeb1937675abe2a9c1e6ef168048
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2023-02-28 21:26:46 -0300

    contexto da api

---
+++ SocialHelpApi/Context/ContextApi.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SocialHelpApi.Models.Entities;
+
+namespace SocialHelpApi.Context
+{
+    public class ContextApi : DbContext
+    {
+        public ContextApi(DbContextOptions<ContextApi> options) : base(options)
+        {
+            
+        }
+            public DbSet<User> Users { get; set; }
+            public DbSet<Message> Messages { get; set; }
+
+        
+    }
+}
