e779ba441a168ae6704616ddd18c87f7a820007f
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2023-03-02 00:09:26 -0300

    mapeados dto

--- SocialHelpApi/Controllers/MessageController.cs
+++ SocialHelpApi/Controllers/MessageController.cs
@@ -7,7 +7,7 @@ using SocialHelpApi.Models.Entities;
 
 namespace Sweeter.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/message")]
     [ApiController]
     public class MessageController : ControllerBase
     {
@@ -29,104 +29,120 @@ namespace Sweeter.Controllers
                 messages = (IOrderedQueryable<Message>)messages.Take(numPosts.Value);
             }
 
-            var sweetResponse = await messages 
+            var messageResponse = await messages
                 .Select(s => _mapper.Map<MessageAllDto>(s))
                 .ToListAsync();
 
-            return Ok(sweetResponse);
+            return Ok(messageResponse);
         }
 
-        // GET: api/message/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Message>> GetMessage(int id, int userId)
+        // GET: api/messages/5
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<IEnumerable<MessageAllDto>>> GetMessagesUser(int userId)
         {
-            var sweet = await _context.DbSetMessages
-                .Where(u => u.Id == id && u.UserId == userId)
-                .FirstOrDefaultAsync();
-
-            if (sweet == null)
+            var messagesUser = await _context.DbSetMessages
+                .Where(s => s.UserId == userId)
+                .OrderByDescending(s => s.DateMessage)
+                .ToListAsync();
+            if (messagesUser == null)
             {
-                return NotFound("Message não localizado");
+                return NotFound("Mensagens não encontradas");
             }
 
-            return sweet;
+            var messageResponse = messagesUser
+                .Select(s => _mapper.Map<MessageAllDto>(s))
+                .ToList();
+
+
+
+
+            return Ok(messageResponse);
         }
 
         // PUT: api/message/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutSweet(int id, int userId, [FromBody] Message sweet)
+        public async Task<IActionResult> PutMessage(
+            int id,
+            int userId,
+            [FromBody] MessageDto message
+        )
         {
-            if (id != sweet.Id || userId != sweet.UserId)
-            {
-                return BadRequest("Você tem que passar o id correto"); //retirar com atualização da dto
-            }
-            var existSweet = await _context.DbSetMessages.FirstOrDefaultAsync(
-                s => s.Id == id && s.UserId == userId
-            );
-            if (existSweet == null)
-            {
-                return BadRequest("Message não encontrado");
-            }
-
-            _context.Entry(sweet).State = EntityState.Modified;
-
             try
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MessageExist(id))
+                var messageUpdate = await _context.DbSetMessages.FirstOrDefaultAsync(
+                    s => s.Id == id && s.UserId == userId
+                );
+                if (messageUpdate == null)
                 {
-                    return NotFound();
+                    return BadRequest("Message não encontrado");
                 }
-                else
-                {
-                    throw;
-                }
-            }
 
-            return NoContent();
+                messageUpdate.Text = message.Text;
+                messageUpdate.DateMessage = DateTime.Now;
+                messageUpdate.Edit = true;
+
+                
+
+                _context.Entry(messageUpdate).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok(messageUpdate);
+            }
+            catch
+            {
+                return StatusCode(500, "Erro interno do servidor");
+            }
         }
 
         // POST: api/message
         [HttpPost]
-        public async Task<ActionResult<MessageDto>> PostSweet(int userId, MessageDto sweetDto)
+        public async Task<ActionResult<MessageDto>> PostSweet(int userId, MessageDto messageDto)
         {
-            var existUser = await _context.DbSetUsers.FirstOrDefaultAsync(s => s.Id == userId);
-            if (existUser == null)
+            try
             {
-                return BadRequest("Usuario não encontrado");
+                var existUser = await _context.DbSetUsers.FirstOrDefaultAsync(s => s.Id == userId);
+                if (existUser == null)
+                {
+                    return BadRequest("Usuario não encontrado");
+                }
+    
+                var message = _mapper.Map<Message>(messageDto);
+                message.UserId = existUser.Id;
+                _context.DbSetMessages.Add(message);
+                await _context.SaveChangesAsync();
+    
+                return Ok(message);
             }
-
-            Message message = new();
-            message.Text = sweetDto.Text;
-            message.UserId = userId;
-            _context.DbSetMessages.Add(message);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetSweet", new { id = message.Id }, message);
+            catch
+            {
+                return StatusCode(500, "Erro interno do servidor");
+            }
         }
 
         // DELETE: api/message/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteSweet(int id)
+        [HttpDelete("{userId}&{id}")]
+        public async Task<IActionResult> DeleteMessage(int userId, int id)
         {
-            var sweet = await _context.DbSetMessages.FindAsync(id);
-            if (sweet == null)
-            {
-                return NotFound();
-            }
-
-            _context.DbSetMessages.Remove(sweet);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+           try
+           {
+             var message = await _context.DbSetMessages
+                 .Where(s => s.UserId == userId && s.Id == id)
+                 .FirstOrDefaultAsync();
+             if (message == null)
+             {
+                 return BadRequest("Mensagem não encontrada");
+             }
+ 
+             _context.DbSetMessages.Remove(message);
+             await _context.SaveChangesAsync();
+ 
+             return Ok("Mensagem excluida com sucesso");
+           }
+           catch
+           {
+                return StatusCode(500, "Erro interno do servidor");
+           }
         }
 
-        private bool MessageExist(int id)
-        {
-            return _context.DbSetMessages.Any(e => e.Id == id);
-        }
+        
     }
 }
