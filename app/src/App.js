32b64a5f4321961b1a8cb27c4360103b6ffc55eb
renanfortkamp <renan_fortkamp@estudante.sesisenai.org.br>
2023-03-02 00:25:18 -0300

    pagina feed basica + contexto para user

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Context from './pages/Context/Context.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Feed from './pages/feed/feed.js';
+import Login from './pages/login/login.js';
+import Register from './pages/register/register.js';
 
 function App() {
+  
+  const [context, setContext] = useState([]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Context.Provider value={[context, setContext]}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Context.Provider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ app/src/pages/feed/feed.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect, useContext } from 'react';
+import Context from '../Context/Context.js';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function Feed() {
+    const [text, setText] = useState('');
+    const [posts, setPosts] = useState([]);
+    const [context, setContext] = useContext(Context);
+    const navigate = useNavigate();
+
+    const updatePost = () => {
+        fetch("http://localhost:5066/api/message")
+            .then(e => e.json())
+            .then(data => {
+                setPosts(data);
+            });
+    }
+
+    const Logout = () => {
+        setContext([]);
+        navigate('/');
+
+    };
+
+    useEffect(() => {
+        updatePost();
+    }, []);
+
+    const postMessage = (e) => {
+        try {
+            e.preventDefault();
+            fetch("http://localhost:5066/api/message?userId=" + context.id, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    text: text,
+                })
+
+
+            })
+                .then(updatePost())
+
+
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    return (
+        <div className='posts' style={{ display: 'flex', justifyContent: "center", alignItems: "center", flexDirection: "column", border: "solid 1px red" }}>
+            <h1>Feed</h1>
+            {<p>Bem vindo {context.name}</p>}
+            <form onSubmit={(e) => { postMessage(e) }}>
+                <input type="text" placeholder="text" onChange={(e) => setText(e.target.value)} />
+                <button type="submit">Submit</button>
+            </form>
+            <button onClick={() => { Logout() }}>Logout</button>
+
+            {posts.map(post => (
+                <div key={post.id} className='post' style={{ border: "solid 1px red", width: "400px" }}>
+                    <div>@{post.userName}</div>
+                    <div>{post.text}</div>
+                    <div>{post.dateMessage}</div>
+                </div>))
+            }
+
+
+
+
+
+        </div>
+
+
+    )
+        ;
+};
